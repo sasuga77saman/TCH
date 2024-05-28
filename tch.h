d6976bd41cabff29d0e9b5a79de5e9177e0aef2d
sasuga77saman <fansu19750412@163.com>
2024-05-28 21:27:50 +0800

    Update tch.h

--- tch.h
+++ tch.h
@@ -1,3 +1,35 @@
-#ifndef _BinaryHeap_H
-#define _BinaryHeap_H
+#ifndef _TCS_H
+#define _TCS_H
+#define WIDTH 40
+#define HEIGHT 30
+int i = 0;
+int score = 0;
+int dirX = 1, dirY = 0;
+//蛇的身体节点坐标结构体 
+struct BODY
+{
+    int x;
+    int y;
+} body;
+//定义的一条蛇，大小与长度 
+struct SNAKE
+{
+    struct BODY body[WIDTH * HEIGHT];
+    int size;
+} snake;
+//食物模块，待完善
+
+
+
+//场景，待完善
+
+
+ 
+void initSnake(void);//初始化蛇 
+void daw(void);//画蛇 
+void input(void);//控制模块 
+void move(void);//移动模块 
+void bodydie(void);// 身体触碰死亡 
+void walldie(void);//墙壁触碰死亡 
+void lenplus(void);//身长增加 
 #endif
