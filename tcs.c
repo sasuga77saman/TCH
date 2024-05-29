6bd92da9a64f2e9b80120cf1ba479957d9ba1d01
s1mpleED <2073806830@qq.com>
2024-05-29 21:52:32 +0800

    Update tcs.c

--- tcs.c
+++ tcs.c
@@ -1,51 +1,51 @@
-#包括 <stdio.h>
-#包括 <conio.h>
-#包括 <windows.h>
-#包括 “tcs.h”
-#包括<时间.h>
-int main（ int argc，char* argv [ ] ） {
-    //隐藏光标
-    控制台光标信息 cursor_info = {  1 ， 0  } ;
-    设置控制台光标信息（获取StdHandle （ STD_OUTPUT_HANDLE ），&cursor_info ）；
-    初始化蛇形线（）；
-    虽然 （1 ） {
-        道( )；
-        输入（） ;
-        移动（） ;
-        //系统（“cls”）；
-        //daw（）；
-        睡眠（100 ）；
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+#include "tcs.h"
+#include<time.h>
+int main(int argc, char* argv[]) {
+    //隐藏光标 
+    CONSOLE_CURSOR_INFO cursor_info = { 1, 0 };
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor_info);
+    initSnake();
+    while (1) {
+        daw();
+        input();
+        move();
+        //system("cls");
+        //daw();
+        Sleep(100);
     }
-    srand（（无符号整数）时间（NULL ））；//设置随机生成起点
-    随机食物（）；//随机生成食物
-    返回 0；
+    srand((unsigned int)time(NULL)); //设置随机数生成起点
+    RandFood(); //随机生成食物
+    return 0;
 }
 
-//蛇模型的初始化，出生位置为场景中心
-void initSnake（ void ） {
-    蛇。尺寸= 3；
-    //snake.body[0]为蛇的头部
-    蛇.身体[ 0 ] 。 x = 宽度 / 2 ;
-    蛇.身体[ 0 ] .y =高度/ 2 ;
+//蛇模型的初始化，出生位置为场景中心 
+void initSnake(void) {
+    snake.size = 3;
+    //snake.body[0]为蛇的头部 
+    snake.body[0].x = WIDTH / 2;
+    snake.body[0].y = HEIGHT / 2;
 
-蛇    。身体[ 1 ] .x =宽度/ 2-1 ;
-    蛇.身体[ 1 ] .y =高度/ 2 ;
+    snake.body[1].x = WIDTH / 2 - 1;
+    snake.body[1].y = HEIGHT / 2;
 
-    蛇.身体[ 2 ] 。 x = 宽度 / 2 - 2 ;
-    蛇.身体[ 2 ] .y =高度/ 2 ;
+    snake.body[2].x = WIDTH / 2 - 2;
+    snake.body[2].y = HEIGHT / 2;
 }
-//画出初始的蛇
-虚空daw (虚空)  {
-    //COORD为conin库中结构体
-    坐标 coord = {  0  } ;
-    我 = 0；
-    对于 （ i；i<snake.size ； i++ ）
+//画出初始的蛇 
+void daw(void) {
+    //COORD为conin库中结构体 
+    COORD coord = { 0 };
+    i = 0;
+    for (i; i < snake.size; i++)
     {
-        坐标.X =蛇。身体[ i ] .x ;
-        坐标.Y =蛇。身体[ i ] .y ;
-        //这串长代码可以改变光标位置
-        设置控制台光标位置（获取StdHandle （ STD_OUTPUT_HANDLE ，坐标）；
-        如果 （ i == 0 ）
+        coord.X = snake.body[i].x;
+        coord.Y = snake.body[i].y;
+        //这串长代码可以改变光标位置 
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+        if (i == 0)
             printf("@");//蛇头部 
         else
             printf("*");//蛇身体 
@@ -62,88 +62,88 @@ void input(void) {
         switch (_getch())//从键盘获得值 
         {
         case 'a':
-            如果 （abs （ dirX ）！= 1 && dirY！= 0 ） {
-                目录X = -1 ;
-                dirY = 0；
+            if (abs(dirX) != 1 && dirY != 0) {
+                dirX = -1;
+                dirY = 0;
             }
-            休息;
+            break;
 
-        案例 ‘d’：
-            如果 （abs （ dirX ）！= 1 && dirY！= 0 ） {
-                目录X = 1 ;
-                dirY = 0；
+        case 'd':
+            if (abs(dirX) != 1 && dirY != 0) {
+                dirX = 1;
+                dirY = 0;
             }
-            休息;
-        案例 ‘w’：
-            如果 （ dirX！= 0 && abs （ dirY ）！= 1 ） {
-                目录X = 0 ;
-                dirY = -1；
+            break;
+        case 'w':
+            if (dirX != 0 && abs(dirY) != 1) {
+                dirX = 0;
+                dirY = -1;
             }
-            休息;
-        案例 's'：
-            如果 （ dirX！= 0 && abs （ dirY ）！= 1 ） {
-                目录X = 0 ;
-                dirY = 1；
+            break;
+        case 's':
+            if (dirX != 0 && abs(dirY) != 1) {
+                dirX = 0;
+                dirY = 1;
             }
-            休息;
+            break;
         }
     }
 }
-无效移动（无效） {
+void move(void) {
 
-    //从尾部开始，将前一个位置赋值给后一个，以达到移动目的
-    lastx = 蛇.身体[蛇.大小- 1 ] 。 x ;
-    lasty = 蛇. body [蛇. size - 1 ] 。 y ;
-    i = 蛇。尺寸-1 ；
-    对于 （ i；i > 0；i-- ）
+    //从尾部开始，将前一个位置赋值给后一个 ，以达到移动的目的 
+    lastx = snake.body[snake.size - 1].x;
+    lasty = snake.body[snake.size - 1].y;
+    i = snake.size - 1;
+    for (i; i > 0; i--)
     {
-        蛇.身体[ i ] 。 x =蛇。身体[ i- 1  ] 。 X ;
-        蛇.身体[ i ] .y = 蛇。身体[ i - 1  ] 。 ；​
+        snake.body[i].x = snake.body[i - 1].x;
+        snake.body[i].y = snake.body[i - 1].y;
     }
-    蛇.身体[  0  ] 。 x + =dirX;
-    蛇.身体[  0  ] 。 y + =方向Y；
+    snake.body[0].x += dirX;
+    snake.body[0].y += dirY;
 
 }
-//未完成的失败机制，
-无效walldie (无效)   {
-    如果 （蛇。身体[  0  ]。x < = 0 || 蛇。身体[  0  ]。x > = 宽度 || 蛇。身体[  0  ]。y < = 0 || 蛇。身体[  0  ]。x > = 高度）
-        printf ( "你死定了！！！" )；
-    返回;
+//尚未完成的死亡机制 ,后续完善 
+void walldie(void) {
+    if (snake.body[0].x <= 0 || snake.body[0].x >= WIDTH || snake.body[0].y <= 0 || snake.body[0].x >= HEIGHT)
+        printf("YOU DIE!!!");
+    return;
 }
-无效 bodydie （无效）{
-    我 = 1；
-    对于（ i；i < snake. 大小； i++ ）{
-        如果 （蛇。身体[  0  ]。x ==蛇。身体[ i ] 。 x &&蛇。身体[  0  ] 。 y ==蛇。身体[ i ] 。 （​
-            printf  (  "你死定了！！！"  )；
-        返回;
+void bodydie(void) {
+    i = 1;
+    for (i; i < snake.size; i++) {
+        if (snake.body[0].x == snake.body[i].x && snake.body[0].y == snake.body[i].y)
+            printf("YOU DIE!!!");
+        return;
     }
 }
-// 光标跳过
-void CursorJump  ( int x, int y )
+//光标跳转
+void CursorJump(int x, int y)
 {
-    坐标正向；//定义光标位置结构体变量
-    正。X =x；//横坐标
-    正。Y =y；//纵坐标
-    句柄句柄 = GetStdHandle  ( STD_OUTPUT_HANDLE ) ; //获取控制台句柄
-    设置光标位置（光标，位置）；//设置光标位置
+    COORD pos; //定义光标位置的结构体变量
+    pos.X = x; //横坐标
+    pos.Y = y; //纵坐标
+    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE); //获取控制台句柄
+    SetConsoleCursorPosition(handle, pos); //设置光标位置
 }
 //颜色设置
-空颜色（ int c ）
+void color(int c)
 {
-    设置控制台文本属性（获取StdHandle （ STD_OUTPUT_HANDLE ），c ）；//颜色设置  
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), c); //颜色设置  
 }
 //随机生成食物
-无效随机食物（）
+void RandFood()
 {
-    int i，j；
-    做
+    int i, j;
+    do
     {
-        // 示例生成食物的横纵坐标
-        i = rand （）％宽度；
-        j = rand （）％高度；
-    }   while   ( face [ i ]  [ j ] != KONG ) ; //确保生成食物的位置为空，若不为空则重新生成
-    面[ i ]  [ j ] = 食物；//将食物位置进行标记
-    颜色（12）；//颜色设置为红色
-    光标跳转（ 2 *j，i ）；//转到储蓄的最佳地点处
-    printf  ( “●” )；//打印食物
+        //随机生成食物的横纵坐标
+        i = rand() % WIDTH;
+        j = rand() % HEIGHT;
+    } while (face[i][j] != KONG); //确保生成食物的位置为空，若不为空则重新生成
+    face[i][j] = FOOD; //将食物位置进行标记
+    color(12); //颜色设置为红色
+    CursorJump(2 * j, i); //光标跳转到生成的随机位置处
+    printf("●"); //打印食物
 }
