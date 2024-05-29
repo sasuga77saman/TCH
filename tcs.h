5744e9d660ce66dd3a60b73871bc8b771b7fd4bd
s1mpleED <2073806830@qq.com>
2024-05-29 21:53:02 +0800

    Update tcs.h

--- tcs.h
+++ tcs.h
@@ -1,27 +1,27 @@
-#如果定义_TCS_H
-#定义_TCS_H
-#定义宽度40
-#定义高度30
+#ifndef _TCS_H
+#define _TCS_H
+#define WIDTH 40
+#define HEIGHT 30
 #define FOOD 2 //标记食物
 #define KONG 0 //标记空（什么也没有）
-int i = 0 ;
-int 分数 = 0；
-int lastx = 0；
-int lasty = 0 ;
-int dirX = 1 , dirY = 0 ;
-//蛇体节点坐标结构
-結構
+int i = 0;
+int score = 0;
+int lastx = 0;
+int lasty = 0;
+int dirX = 1, dirY = 0;
+//蛇的身体节点坐标结构体 
+struct BODY
 {
-    int x；
-    int y；
-身体；
-//定义的一条蛇，大小与长度
-結構蛇形
+    int x;
+    int y;
+} body;
+//定义的一条蛇，大小与长度 
+struct SNAKE
 {
-    结构主体[宽度*高度]；
-
-}蛇;
-int 面[宽度] [高度] ; //标记游戏各个位置状态
+    struct BODY body[WIDTH * HEIGHT];
+    int size;
+} snake;
+int face[WIDTH][HEIGHT]; //标记游戏区各个位置的状态
 //食物模块，待完善
 
 
@@ -29,15 +29,15 @@ int 面[宽度] [高度] ; //标记游戏各个位置状态
 //场景，待完善
 
 
-无效隐藏光标（） ; //隐藏光标
-空值颜色（ int c ）；//颜色设置
-void CursorJump ( int x, int y )；// 光标跳转
-无效随机食物（） ; //随机生成食物
-void initSnake （ void ）；//初始化蛇
-void daw ( void ) ; //画蛇
-无效输入（无效）；//控制模块
-无效移动（无效）；//移动模块
-无效bodydie  (无效) ; //身体碰撞失败
-无效walldie (无效) ; //华尔街日报失败
-void lenplus  ( void ) ; //主席
-＃万一 #pragma once
+void HideCursor();//隐藏光标
+void color(int c);//颜色设置
+void CursorJump(int x, int y);//光标跳转
+void RandFood();//随机生成食物
+void initSnake(void);//初始化蛇 
+void daw(void);//画蛇 
+void input(void);//控制模块 
+void move(void);//移动模块 
+void bodydie(void);// 身体触碰死亡 
+void walldie(void);//墙壁触碰死亡 
+void lenplus(void);//身长增加 
+#endif#pragma once
