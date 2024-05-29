49fca6941ea21a282dab3d99648ab716c2b1ac7c
2023402228 <1299749420@qq.com>
2024-05-30 01:23:33 +0800

    Update tch1.c

--- tch1.c
+++ tch1.c
@@ -1,114 +1,158 @@
+
+//由dev c++编辑，如整体代码无法运行，请拆分使用 
 #include <stdio.h>
-#include <stdlib.h>
-#include <graphics.h>		
+#include<string.h>
+#include<stdlib.h>
 #include <conio.h>
+#include <windows.h>a
+#include<graphics.h>//该文件库无法识别的，应该是没有安装eazy-X，删掉这一串就好了
+#include<time.h>
+#include<math.h>
+#include "tcs.h"
+int main(int argc, char* argv[]) {
+    //隐藏光标 
+    CONSOLE_CURSOR_INFO cursor_info = { 1, 0 };
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor_info);
+    initSnake();
+    initFoood();
+    //snake.size += 10;//仅为测试bodydie使用
+    while (1) {
+        walldie();
+        bodydie();
+        daw();
+        input();
+        move();
+        Sleep(200);//Sleep为暂时替代速度调试
+        //speed_change();
 
-#define SNAKE_NUM  500
-enum DIR
-{
-	UP,
-	DOWN,
-	LEFT,
-	RIGHT,
-};
-struct Snacke
-{
-	int size;
-	int dir;
-	int speed;
-	POINT coor[SNAKE_NUM]; 
-}snake;
-void GameInit()
-{
-	initgraph(640, 480, SHOWCONSOLE);
-	snake.size = 3;
-	snake.speed = 10;
-	snake.dir=RIGHT;
-	for (int i = 0; i < snake.size; i++)
-	{
-		snake.coor[i].x = 40-10*i;
-		snake.coor[i].y = 10;
-		printf("%d%d", snake.coor[i].x, snake.coor[i].y);
-	}
-
+    }
+    return 0;
 }
-void GameDraw()
-{
-	BeginBatchDraw();
-	setbkcolor(RGB(28, 115, 119));
-	cleardevice();
-	setfillcolor(RED);
-	for (int i = 0; i < snake.size; i++)
-	{
-		solidcircle(snake.coor[i].x, snake.coor[i].y, 5);
-	}
-	EndBatchDraw();
-}
+//蛇模型的初始化，出生位置为场景中心 
+void initSnake(void) {
+    initgraph(640, 480, SHOWCONSOLE);
+    snake.size = 3;
+    //snake.body[0]为蛇的头部 
+    snake.body[0].x = WIDTH / 2;
+    snake.body[0].y = HEIGHT / 2;
 
-void snakemove()
-{
-	for (int i = 0; i < snake.size; i++)
-	{
-		switch (snake.dir) 
-		{
-		case UP:
-			snake.coor[i].y--;
-			break;
-		case DOWN:
-			snake.coor[i].y++;
-			break;
-		case LEFT:
-			snake.coor[i].x--;
-			break;
-		case RIGHT:
-			snake.coor[i].x++;
-			break;
-		}
-	}
-}
+    snake.body[1].x = WIDTH / 2 - 1;
+    snake.body[1].y = HEIGHT / 2;
 
-void keyControl()
-{
-	if (_kbhit())
-	{
-		switch (_getch())
-		{
-		case'w':
-		case'W':
-		case 72:
-			snake.dir = UP;
-			break;
-		case'a':
-		case'A':
-		case 80:
-			snake.dir = DOWN;
-			break;
-		case's':
-		case'S':
-		case 75:
-			snake.dir = LEFT;
-			break;
-		case'd':
-		case'D':
-		case 77:
-			snake.dir = RIGHT;
-			break;
-		}
-	}
+    snake.body[2].x = WIDTH / 2 - 2;
+    snake.body[2].y = HEIGHT / 2;
+}
+//画出初始的蛇 
+void daw(void) {
+    //COORD为conin库中结构体 
+    COORD coord = { 0 };
+    i = 0;
+    for (i; i < snake.size; i++)
+    {
+        coord.X = snake.body[i].x;
+        coord.Y = snake.body[i].y;
+        
+        //这串长代码可以改变光标位置 
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+        
+        if (i == 0)
+            printf("@");//蛇头部 
+        else
+            printf("*");//蛇身体 
+    }
+    
+
+    coord.X = food.X;
+    coord.Y = food.Y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+    printf("#");
+    
+    coord.X = 0;
+    coord.Y = HEIGHT+2;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
+void initFoood(void){
+    food.X = rand() % WIDTH;
+    food.Y = rand() % HEIGHT;
+    return ;
+}
 
-int main()
+void input(void) {
+    //_kbhit函数确认是否从键盘获取值，获得输出真；反之亦然 
+    if (_kbhit())
+    {
+        switch (_getch())//从键盘获得值 
+        {
+        case 'a':
+            if (abs(dirX) != 1 && dirY != 0) {
+                dirX = -1;
+                dirY = 0;
+            }
+            break;
+
+        case 'd':
+            if (abs(dirX) != 1 && dirY != 0) {
+                dirX = 1;
+                dirY = 0;
+            }
+            break;
+        case 'w':
+            if (dirX != 0 && abs(dirY) != 1) {
+                dirX = 0;
+                dirY = -1;
+            }
+            break;
+        case 's':
+            if (dirX != 0 && abs(dirY) != 1) {
+                dirX = 0;
+                dirY = 1;
+            }
+            break;
+        }
+    }
+}
+void move(void) {
+
+    //从尾部开始，将前一个位置赋值给后一个 ，以达到移动的目的 
+    lastx = snake.body[snake.size - 1].x;
+    lasty = snake.body[snake.size - 1].y;
+    i = snake.size - 1;
+    for (i; i > 0; i--)
+    {
+        snake.body[i].x = snake.body[i - 1].x;
+        snake.body[i].y = snake.body[i - 1].y;
+    }
+    snake.body[0].x += dirX;
+    snake.body[0].y += dirY;
+
+}
+//尚未完成的死亡机制 ,后续完善 
+void walldie(void) {
+    if (snake.body[0].x <= 0 || snake.body[0].x >= WIDTH || snake.body[0].y <= 0 || snake.body[0].y >= HEIGHT) {
+        printf("YOU DIE!!!");
+        exit(0);
+    }
+}
+void bodydie(void) {
+    i = 1;
+    for (i; i < snake.size; i++) {
+        if (snake.body[0].x == snake.body[i].x && snake.body[0].y == snake.body[i].y) {
+            printf("YOU DIE!!!");
+            exit(0);
+        }
+    }
+}
+void speed_change(void) {
+    if (1) {
+        Sleep(snake.speed);
+        snake.speed -= 3;
+    }
+}
+int main(void)
 {
-	GameInit();
-	GameDraw();
-	
-	while (1)
-	{
-		GameDraw();
-		snakemove();
-		keyControl();
-		Sleep(20);
-	}
-	return 0;
+    srand(time(NULL));
+
+    system("pause");
+    return EXIT_SUCCESS;
 }
