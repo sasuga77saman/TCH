541209f06fc41d62ee2e2156c0d0fc35c825af5a
2023402228 <1299749420@qq.com>
2024-05-30 16:23:55 +0800

    Create tch1.h

---
+++ tch1.h
@@ -0,0 +1,78 @@
+#pragma once
+#pragma once
+#pragma once
+#ifndef _TCS_H
+#define _TCS_H
+#define WIDTH 70
+#define HEIGHT 30
+int i = 0;
+int s = 0;
+int score = 0;
+int lastx = 0;
+int lasty = 0;
+int dirX = 1, dirY = 0;
+struct FOOD1
+{
+	int x;
+	int y;
+}food1;
+
+struct FOOD2
+{
+	int x;
+	int y;
+}food2;
+
+struct FOOD3
+{
+	int x;
+	int y;
+}food3;
+
+struct FOOD4
+{
+	int x;
+	int y;
+}food4;
+
+struct FOOD5
+{
+	int x;
+	int y;
+}food5;
+//蛇的身体节点坐标结构体 
+struct BODY
+{
+	int x;
+	int y;
+} body;
+//定义的一条蛇，大小与长度 
+struct SNAKE
+{
+	struct BODY body[WIDTH * HEIGHT];
+	int size;
+	int speed = 200;
+} snake;
+
+
+
+//场景，待完善
+
+
+
+void initSnake(void);//初始化蛇 
+void initWall(void);
+void daw(void);//画蛇 
+void input(void);//控制模块 
+void move(void);//移动模块 
+void bodydie(void);// 身体触碰死亡 
+void walldie(void);//墙壁触碰死亡 
+void speed_change(void);//速度改变
+void sp(void);
+void eat_food(void);//统计分数
+void food1_sc(void);//食物生成
+void food2_sc(void);
+void food3_sc(void);
+void food4_sc(void);
+void food5_sc(void);
+#endif
