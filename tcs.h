0abeb88a79ec4b83aa15076770b3eea1083d1ded
s1mpleED <2073806830@qq.com>
2024-05-30 13:27:09 +0800

    Update tcs.h

--- tcs.h
+++ tcs.h
@@ -1,43 +1,65 @@
 #ifndef _TCS_H
 #define _TCS_H
-#define WIDTH 40
-#define HEIGHT 30
-#define FOOD 2 //标记食物
-#define KONG 0 //标记空（什么也没有）
+#define WIDTH 65
+#define HEIGHT 28
 int i = 0;
+int s = 0;
 int score = 0;
 int lastx = 0;
 int lasty = 0;
 int dirX = 1, dirY = 0;
+struct FOOD
+{
+	int x;
+	int y;
+}food;
+struct FOOD1
+{
+	int x;
+	int y;
+}food1;
+struct FOOD2
+{
+	int x;
+	int y;
+}food2;
+struct FOOD3
+{
+	int x;
+	int y;
+}food3;
 //蛇的身体节点坐标结构体 
 struct BODY
 {
-    int x;
-    int y;
+	int x;
+	int y;
 } body;
 //定义的一条蛇，大小与长度 
 struct SNAKE
 {
-    struct BODY body[WIDTH * HEIGHT];
-    int size;
+	struct BODY body[WIDTH * HEIGHT];
+	int size;
+	int speed = 200;
 } snake;
-int face[WIDTH][HEIGHT]; //标记游戏区各个位置的状态
-//食物模块，待完善
 
 
 
 //场景，待完善
 
 
-void HideCursor();//隐藏光标
-void color(int c);//颜色设置
-void CursorJump(int x, int y);//光标跳转
-void RandFood();//随机生成食物
+
 void initSnake(void);//初始化蛇 
+void initWall(void);
 void daw(void);//画蛇 
 void input(void);//控制模块 
 void move(void);//移动模块 
 void bodydie(void);// 身体触碰死亡 
 void walldie(void);//墙壁触碰死亡 
-void lenplus(void);//身长增加 
-#endif#pragma once
+void speed_change(void);//速度改变
+void sp(void);
+void eat_food(void);//统计分数
+void food_sc(void);//食物生成
+void food1_sc(void);
+void food2_sc(void);
+void food3_sc(void);
+#endif
